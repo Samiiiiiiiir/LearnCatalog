d70a5965f0bef354b41603b34b287c054b7ed12d
Samir <sam1r.aslanoff.as@gmail.com>
2025-05-25 20:19:40 +0300

    add sass, stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-order-config-standard',
+  ],
+  plugins: ['stylelint-order'],
+};
