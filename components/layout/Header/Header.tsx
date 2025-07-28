984f56a981f1c5398eca04fc2e5fd430cbc8cfe8
Samir <sam1r.aslanoff.as@gmail.com>
2025-07-29 00:36:24 +0300

    update mobile menu behavior

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -4,7 +4,7 @@ import { DetailedHTMLProps, HTMLAttributes, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { ButtonIcon, Logo } from '@/components';
 import { motion } from 'framer-motion';
-import { usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { IMenuItem } from '@/types';
 import { Sidebar } from '../Sidebar/Sidebar';
 
@@ -29,11 +29,13 @@ const sidebarVariants = {
 
 export const Header = ({ data, className, ...props }: HeaderProps) => {
   const [isMenuClosed, setIsMenuClosed] = useState(false);
-  const path = usePathname();
+  const { alias } = useParams();
 
   useEffect(() => {
-    setIsMenuClosed(false);
-  }, [path]);
+    if (alias) {
+      setIsMenuClosed(false);
+    }
+  }, [alias]);
 
   return (
     <header className={clsx(styles.header, className)} {...props}>
@@ -51,7 +53,7 @@ export const Header = ({ data, className, ...props }: HeaderProps) => {
         animate={isMenuClosed ? 'opened' : 'closed'}
         className={styles.mobileMenu}
       >
-        <Sidebar data={data} />
+        <Sidebar className={styles.mobileList} data={data} />
         <ButtonIcon
           onClick={() => setIsMenuClosed(false)}
           className={styles.close}
